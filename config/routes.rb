c1deeaceb640901d39009194250783e077bc75a5
Pedro Agostinho <pedroagosti@gmail.com>
2019-02-22 10:58:33 +0000

    Mister Cocktail

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cocktails, only: %i[index new create show] do
+    resources :doses, only: %i[new create]
+  end
+
+  resources :doses, only: %i[destroy]
+
+  root to: 'cocktails#index'
 end
